a650809a7ba551f8caf708f76745617a04b6c317
Bruna <bruna.dall9741@gmail.com>
2022-10-11 15:00:55 -0300

    Criação da Estrutura de Diretórios

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import "reflect-metadata"
 import express from "express"
 
-
+import usersRoutes from "./routes/users"
+import loginRouter from "./routes/login"
 
 const app = express()
 app.use(express.json())
 
+app.use("/users", usersRoutes)
+app.use("/login", loginRouter)
 
 export default app
